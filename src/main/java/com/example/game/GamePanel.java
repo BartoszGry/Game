c32ad8087ddf905c2d814e425a18ff370b88af72
BartoszGry <bartoszgrygierczyk01@gmail.com>
2023-07-25 21:11:05 +0200

    initial commit

---
+++ src/main/java/com/example/game/GamePanel.java
@@ -0,0 +1,19 @@
+package com.example.game;
+
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+
+public class GamePanel extends Scene {
+    public GamePanel(Parent parent, int i, int i1) {
+        super(parent,i,i1);
+        this.setFill(Color.BLACK);
+    }
+
+    public void setupGame(){
+
+    }
+}
