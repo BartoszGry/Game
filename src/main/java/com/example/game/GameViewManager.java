b7584d4f2b0b8434f2fd830469360626d46e02bb
BartoszGry <bartoszgrygierczyk01@gmail.com>
2023-08-28 14:10:26 +0200

    dodany stateczek

--- src/main/java/com/example/game/GameViewManager.java
+++ src/main/java/com/example/game/GameViewManager.java
@@ -1,11 +1,19 @@
 package com.example.game;
 
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class GameViewManager {
     private AnchorPane gamePane;
+    private Node ship;
+    boolean goLeft,goRight,idle;
+    private static final String SHIP_IMAGE_LOC ="C:\\Users\\beneh\\intelij\\Game\\src\\main\\resources\\images\\spaceship.png";
+    private Image spaceShipImg;
     private Scene gameScene;
     public Stage gameStage;
     private static final int GAME_WIDTH=600;
@@ -16,10 +24,19 @@ public class GameViewManager {
 
     public GameViewManager(){
         initializeStage();
+        setShip();
         gameScene.setOnKeyPressed(keyH);
         gameScene.setOnKeyReleased(keyH);
     }
 
+    public void setShip(){
+        spaceShipImg=new Image(SHIP_IMAGE_LOC);
+        ship=new ImageView(spaceShipImg);
+        Group level = new Group(ship);
+        gamePane.getChildren().add(level);
+        ship.setLayoutX(252);
+        ship.setLayoutY(650);
+    }
     public void initializeStage(){
         gamePane=new AnchorPane();
         gameScene= new Scene(gamePane,GAME_WIDTH,GAME_HEIGHT);
