5d4aa612409778c3c03ddaea55680e0be4964c7f
BartoszGry <bartoszgrygierczyk01@gmail.com>
2023-08-28 21:59:58 +0200

    generic stateczek

--- src/main/java/com/example/entity/Player.java
+++ src/main/java/com/example/entity/Player.java
@@ -1,4 +1,14 @@
 package com.example.entity;
 
-public class Player {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Player extends ImageView {
+    private static final String SHIP_IMAGE_LOC ="C:\\Users\\beneh\\intelij\\Game\\src\\main\\resources\\images\\spaceship.png";
+    private Image spaceShipImg;
+    public Player() {
+        super();
+        this.spaceShipImg=new Image(SHIP_IMAGE_LOC);
+        setImage(spaceShipImg);
+    }
 }
--- src/main/java/com/example/game/GameViewManager.java
+++ src/main/java/com/example/game/GameViewManager.java
@@ -1,41 +1,35 @@
 package com.example.game;
 
+import com.example.entity.Player;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class GameViewManager {
     private AnchorPane gamePane;
-    private Node ship;
+    private Group level;
     boolean goLeft,goRight,idle;
-    private static final String SHIP_IMAGE_LOC ="C:\\Users\\beneh\\intelij\\Game\\src\\main\\resources\\images\\spaceship.png";
-    private Image spaceShipImg;
+    public Player player;
     private Scene gameScene;
     public Stage gameStage;
     private static final int GAME_WIDTH=600;
     private static final int GAME_HEIGHT=800;
     KeyHandler keyH=new KeyHandler();
-    private Stage menuP;
-
 
     public GameViewManager(){
         initializeStage();
-        setShip();
+        setPlayer();
         gameScene.setOnKeyPressed(keyH);
         gameScene.setOnKeyReleased(keyH);
     }
 
-    public void setShip(){
-        spaceShipImg=new Image(SHIP_IMAGE_LOC);
-        ship=new ImageView(spaceShipImg);
-        Group level = new Group(ship);
+    public void setPlayer(){
+        player=new Player();
+        level=new Group(player);
         gamePane.getChildren().add(level);
-        ship.setLayoutX(252);
-        ship.setLayoutY(650);
+        player.setLayoutX(252);
+        player.setLayoutY(650);
     }
     public void initializeStage(){
         gamePane=new AnchorPane();
